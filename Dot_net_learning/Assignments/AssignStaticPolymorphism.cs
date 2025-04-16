d9fa19ce74d8854720d3836f13960a9131b5fd1e
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2025-04-16 12:44:14 +0530

    Assignment on Static Polymorphism

---
+++ Dot_net_learning/Assignments/AssignStaticPolymorphism.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_learning.Assignments
+{
+    class Calculator
+    {
+        public void calculate(int a, int b)
+        {
+            Console.WriteLine($"Sum of {a} and {b} is: {a + b}");
+            Console.WriteLine($"Sub of {a} and {b} is: {a - b}");
+            Console.WriteLine($"Mul of {a} and {b} is: {a * b}");
+            Console.WriteLine($"Div of {a} and {b} is: {a / b}");
+            Console.WriteLine();
+        }
+        public void calculate(double a, int b)
+        {
+            Console.WriteLine($"Sum of {a} and {b} is: {a + b}");
+            Console.WriteLine($"Sub of {a} and {b} is: {a - b}");
+            Console.WriteLine($"Mul of {a} and {b} is: {a * b}");
+            Console.WriteLine($"Div of {a} and {b} is: {a / b}");
+        }
+    }
+    internal class AssignStaticPolymorphism
+    {
+        static void Main()
+        {
+            Calculator calc = new Calculator();
+            calc.calculate(4, 2);
+            calc.calculate(6.2, 2);
+        }
+    }
+}
