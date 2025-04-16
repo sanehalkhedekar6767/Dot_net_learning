c702277fce1bceb4446a844b9fdbeeba50ce9625
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2025-04-16 12:45:25 +0530

    Assignment on Dynamic polymorphism

---
+++ Dot_net_learning/Assignments/AssignDynamicPolymorphism.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_learning.Assignments
+{
+
+    //Base class
+    class Vehicle
+    {
+        public virtual void StartEngine()
+        {
+            Console.WriteLine("Vehicle engine started");
+        }
+    }
+
+    //Derived class
+    class Car: Vehicle
+    {
+        public override void StartEngine()
+        {
+            Console.WriteLine("Car engine started");
+        }
+    }
+
+    class Truck: Vehicle
+    {
+        public override void StartEngine()
+        {
+            Console.WriteLine("Truck engine started");
+        }
+    }
+
+
+
+
+    internal class AssignDynamicPolymorphism
+    {
+        static void Main()
+        {
+            Vehicle vehicle = new Vehicle();
+            Vehicle car = new Car();
+            Vehicle truck = new Truck();
+
+            //calling methods
+            vehicle.StartEngine();
+            car.StartEngine();
+            truck.StartEngine();
+
+
+        }
+    }
+}
