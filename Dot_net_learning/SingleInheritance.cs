b6281a35b42fadd6b3edff2fa37aaed11ccf9b2c
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2025-04-28 12:35:25 +0530

    Single Inheritance in C#

---
+++ Dot_net_learning/SingleInheritance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_learning
+{
+
+    //Base Class or parent class or super class
+    class Vehicle
+    {
+        public string Brand { get; set; }
+        public void Drive()
+        {
+
+            Console.WriteLine($"{Brand} is driving");
+
+        }
+    }
+
+        //child class or derived class or sub class
+        class Car : Vehicle
+        {
+            public int NumberOfDoor { get; set; }
+
+            public void ShowCarInfo()
+            {
+                Console.WriteLine($"Brand: {Brand} and Number of Doors: {NumberOfDoor}");
+            }
+
+
+        }
+
+
+
+        internal class SingleInheritance
+        {
+            static void Main()
+            {
+                Car myCar = new Car();
+            myCar.Brand = "BMW";
+            myCar.NumberOfDoor = 4;
+            myCar.Drive();
+            myCar.ShowCarInfo();
+
+        }
+        }
+    }
