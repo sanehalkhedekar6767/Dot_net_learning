bbf0bc1768fd056a52f0cc14ad5fea735be69bc0
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2025-04-28 13:17:03 +0530

    Assignment on interface in C#

---
+++ Dot_net_learning/Assignments/AssignInterface.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_learning.Assignments
+{
+    internal interface IPlayable
+    {
+        void Play();
+    }
+
+    class VideoPlayer : IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing Video.....");
+        }
+
+    }
+
+    class MusicPlayer: IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing Music....");
+        }
+    }
+
+
+    internal interface AssignInterface
+    {
+        static void Main()
+        {
+            IPlayable vd = new VideoPlayer();
+            vd.Play();
+            IPlayable ms = new MusicPlayer();
+            ms.Play();
+
+        }
+    }
+}
