dc023edf13fca2d1eeb545f90eed81c30dba7fe1
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2025-04-28 12:36:00 +0530

    Multiple Inheritance in C#

---
+++ Dot_net_learning/MultilevelInheritance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_learning
+{
+
+    class A //Base class
+    {
+        public void start()
+        {
+            Console.WriteLine("Vehicle Started");
+
+        }
+
+    }
+
+    class B : A //Derived class and Base class
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car is Driving");
+        }
+
+    }   
+
+    class C : B //Derived class
+    {
+        public void TroubleShoot()
+        {
+            Console.WriteLine("The sport is in another mode");
+        }
+
+    }
+
+    internal class MultilevelInheritance
+    {
+        static void Main()
+        {
+
+            C c = new C(); //Creating object of derived class
+            c.start(); //Calling method of base class
+            c.Drive(); //Calling method of derived class
+            c.TroubleShoot(); //Calling method of derived class
+        }
+    }
+}
