99ca40001c495d20e7408e3ca8661f084ef3a21b
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2025-08-26 11:43:42 +0530

    Sealed and static class

---
+++ Dot_net_learning/SealedClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_learning
+{
+    //Base Class
+    class Employee
+    {
+        public virtual void Role()
+        {
+            Console.WriteLine("I am an employee");
+        }
+    }
+
+    //sealed class that prevent inheritance
+     sealed class Manager:Employee
+    {
+        public override void Role()
+        {
+            Console.WriteLine("I am a manager");
+        }
+    }
+
+    //attempting to inherit from sealed class - will cause compiler error
+    //class TeamLead : Manager
+    //{
+
+    //}
+
+
+    internal class SealedClass
+    {
+        static void Main()
+        {
+            Manager mg = new Manager();
+            mg.Role();
+        }
+    }
+}
---
+++ Dot_net_learning/StaticClass.cs
@@ -0,0 +1,51 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Dot_net_learning
+//{
+//    //static class
+//    static class Product
+//    {
+//        //static member
+//        public static int productId;
+//        public static string productName;
+//        public static int productPrice;
+
+//        //static constructor
+//        static Product()
+//        {
+//            productId = 101;
+//            productName = "Demo";
+//            productPrice = 1000;
+//        }
+
+//        //static method
+//        public static void GetProductDetails()
+//        {
+//            Console.WriteLine($"ProductId: {productId}, ProductName: {productName}, ProductPrice: {productPrice}");
+//        }
+
+//        public static void GetDiscount()
+//        {
+//            int discountAmount = productPrice / 10;
+//            Console.WriteLine("Your discount amount is ", discountAmount);
+//            Console.WriteLine("Total amount of product is ", (productPrice - discountAmount));
+
+
+//        }
+//    }
+//    internal class StaticClass
+//    {
+//        static void Main()
+//        {
+
+//            //Product.GetProductDetails();
+//            //Product.GetDiscount();
+//            //Console.ReadLine();
+
+//        }
+//    }
+//}
