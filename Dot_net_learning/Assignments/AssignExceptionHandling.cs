d5c9281338e28201ba090ae378ed95662b24bb0f
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2025-08-26 11:48:36 +0530

    Exception Handling in C#

---
+++ Dot_net_learning/Assignments/AssignExceptionHandling.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_learning.Assignments
+{
+    internal class AssignExceptionHandling
+    {
+        static void Main()
+        { 
+            try
+            {
+                Console.WriteLine("--------------Assignment 1----------------");
+                Console.Write("Enter value of num1: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\t");
+                Console.Write("Enter value of num2: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                double sum, diff, mul, div;
+                sum = num1 + num2;
+                diff = num1 - num2;
+                mul = num1 * num2;
+                div = num1 / num2;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            catch (ArithmeticException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+        }
+    }
+
+
+  
+}
---
+++ Dot_net_learning/ExceptionHandling.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_learning
+{
+    internal class ExceptionHandling
+    {
+        static void Main()
+        {
+
+            //try :- it executes always, alone not allowed
+            //catch :-it execute once error is occured, we can use multiple catch block, compulsory with try block
+            //finally :-it executes always either error occurred or not, not compulsory
+
+            int[] numbers = { 1, 2, 3 };
+            int index = 3;
+            try
+            {
+                int value = numbers[index];
+                Console.WriteLine($"Value at index {index} is {value}");
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"An error occured for index out of bound: {ex.Message}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"An erro occured: {ex.Message}");    
+            }
+            finally
+            {
+                Console.WriteLine("This code always execute");
+            }
+
+
+            //Divide by Zero
+            int num = 10;
+            int deno = 0;
+            double result = 0;
+            try
+            {
+                result = num / deno;
+                Console.WriteLine($"Result of division is: {result}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message} cannot be divide by zero");
+            } 
+
+        }
+    }
+}
